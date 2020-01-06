a6ce00a37ef80896be05fe7f3a820ee289e78ddd
4myc <missch3n@gmail.com>
2020-01-05 20:25:01 -0800

    Done.

--- index.js
+++ index.js
@@ -14,11 +14,3 @@ function countDown(number) {
     }
     console.log(number);
 }
-
-// ALTERNATIVE:
-// function countDown(number) {
-//     for (let i = 9; i >= 0; i--) {
-//         console.log([i]);
-//     }
-//     console.log(number);
-// }
