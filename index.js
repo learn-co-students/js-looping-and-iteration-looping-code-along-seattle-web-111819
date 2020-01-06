cd705fce581154dce83317dc881f0512363af084
Alice Won <alicewon4@gmail.com>
2020-01-05 22:34:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+// const names = ["Ada", "Brendan", "Ali"]
+// let event = "birthday"
+
+
+
+function writeCards(names, event) {
+  let thankYouCards = []
+  for (let i=0; i < names.length; i++) {
+  thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards;
+}
+
+
+function countDown(number) {
+  while (number > 0) {
+    console.log(number);
+    number -= 1;
+  }
+  console.log(number);
+}
