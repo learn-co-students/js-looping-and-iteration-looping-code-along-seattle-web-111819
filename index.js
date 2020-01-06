9a2ef6e606fed67e5732dce887c433236fc7c262
seanpgallivan <seanpgallivan@hotmail.com>
2020-01-05 21:35:15 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let thanks = [];
+function writeCards(array, msg) {
+    for (let i = 0; i < array.length; i++) {
+        thanks.push(`Thank you, ${array[i]}, for the wonderful ${msg} gift!`)
+    }
+    return thanks
+}
+
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num);
+        num--;
+    }
+}
